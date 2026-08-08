4395724075016ba38793841aaa82da248cd821b1
gurori <nazarovslava124@gmail.com>
2026-08-08 16:03:38 +0300

    fix: declare properties as nullable

--- ites.Core/Models/User.cs
+++ ites.Core/Models/User.cs
@@ -1,22 +1,21 @@
-﻿namespace ites.Core.Models
+﻿namespace ites.Core.Models;
+
+public class User(Guid id, string firstName, string email, string passwordHash, string role)
 {
-    public class User(Guid id, string firstName, string email, string passwordHash, string role)
-    {
-        public Guid Id { get; private set; } = id;
-        public string FirstName { get; private set; } = firstName;
-        public string LastName { get; private set; }
-        public string MiddleName { get; private set; }
-        public string Email { get; private set; } = email;
-        public string PasswordHash { get; private set; } = passwordHash;
-        public string Role { get; private set; } = role;
-        public string Description { get; private set; }
-        public string JobTitle { get; private set; }
-        public IList<Guid> CompetitionsIds { get; private set; }
-        public IList<Guid> OrdersIds { get; private set; }
-        public IList<Guid> ApplicationsForCompetitions { get; private set; }
-        public IList<Guid> ApplicationsForOrders { get; private set; }
-        public IList<Guid> ApplicationsForTeams { get; private set; }
-        public IList<Guid> ApplicationsIds { get; private set; }
-        public Guid? TeamId { get; private set; }
-    }
+    public Guid Id { get; private set; } = id;
+    public string FirstName { get; private set; } = firstName;
+    public string? LastName { get; private set; }
+    public string? MiddleName { get; private set; }
+    public string Email { get; private set; } = email;
+    public string PasswordHash { get; private set; } = passwordHash;
+    public string Role { get; private set; } = role;
+    public string? Description { get; private set; }
+    public string? JobTitle { get; private set; }
+    public IList<Guid>? CompetitionsIds { get; private set; }
+    public IList<Guid>? OrdersIds { get; private set; }
+    public IList<Guid>? ApplicationsForCompetitions { get; private set; }
+    public IList<Guid>? ApplicationsForOrders { get; private set; }
+    public IList<Guid>? ApplicationsForTeams { get; private set; }
+    public IList<Guid>? ApplicationsIds { get; private set; }
+    public Guid? TeamId { get; private set; }
 }
