13b2e481ba617ff12f40bf0d2e4b7ea6f72f0db7
gurori <nazarovslava124@gmail.com>
2026-08-08 16:13:50 +0300

    fix: add default values

--- ites.Application/Contracts/Teams/TeamResponse.cs
+++ ites.Application/Contracts/Teams/TeamResponse.cs
@@ -1,30 +1,29 @@
 ﻿using ites.Application.Contracts.Users;
 
-namespace ites.Application.Contracts.Teams
+namespace ites.Application.Contracts.Teams;
+
+public class TeamResponse
 {
-    public class TeamResponse
+    public TeamResponse() { }
+
+    public TeamResponse(
+        Guid id,
+        string name,
+        string description,
+        IList<UserProfileResponse> members,
+        Guid adminId
+    )
     {
-        public TeamResponse() { }
-
-        public TeamResponse(
-            Guid id,
-            string name,
-            string description,
-            IList<UserProfileResponse> members,
-            Guid adminId
-        )
-        {
-            Id = id;
-            Name = name;
-            Description = description;
-            Members = members;
-            AdminId = adminId;
-        }
-
-        public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public IList<UserProfileResponse> Members { get; private set; }
-        public Guid AdminId { get; private set; }
+        Id = id;
+        Name = name;
+        Description = description;
+        Members = members;
+        AdminId = adminId;
     }
+
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public IList<UserProfileResponse> Members { get; private set; } = [];
+    public Guid AdminId { get; private set; }
 }
