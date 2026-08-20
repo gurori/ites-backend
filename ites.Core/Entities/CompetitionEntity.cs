16fde6befddb69208a6ecfb9cc6929560c41f942
gurori <nazarovslava124@gmail.com>
2026-08-20 16:58:24 +0300

    refactor: remove unused code

--- ites.Core/Entities/CompetitionEntity.cs
+++ ites.Core/Entities/CompetitionEntity.cs
@@ -4,11 +4,6 @@
     {
         public Guid Id { get; set; }
         public string ContentInHtml { get; set; } = string.Empty;
-
-        // public string Title { get; set; } = string.Empty;
-        // public string Description { get; set; } = string.Empty;
-        // public DateTime StartDate { get; set; }
-        // public DateTime EndDate { get; set; }
         public IList<Guid> MembersIds { get; set; } = [];
         public IList<Guid> OrganizersIds { get; set; } = [];
     }
