ee8e068bc5cdbd23a59e86cb56ab57f94a66c7fb
gurori <nazarovslava124@gmail.com>
2026-08-21 08:21:54 +0300

    refactor: update naming

--- ites.Core/Interfaces/Repositories/IRequestEntityRepository.cs
+++ ites.Core/Interfaces/Repositories/IRequestEntityRepository.cs
@@ -5,12 +5,12 @@ namespace ites.Core.Interfaces.Repositories;
 public interface IRequestEntityRepository
 {
     public Task CreateForCompetitionAsync(
-        RequestEntity application,
+        RequestEntity requestEntity,
         CancellationToken ct = default
     );
     public Task HandleCompetitionAsync(Guid id, bool isAccept, CancellationToken ct = default);
-    public Task CreateForOrderAsync(RequestEntity application, CancellationToken ct = default);
+    public Task CreateForOrderAsync(RequestEntity requestEntity, CancellationToken ct = default);
     public Task HandleOrderAsync(Guid id, bool isAccept, CancellationToken ct = default);
-    public Task CreateForTeamAsync(RequestEntity application, CancellationToken ct = default);
+    public Task CreateForTeamAsync(RequestEntity requestEntity, CancellationToken ct = default);
     public Task HandleTeamAsync(Guid id, bool isAccept, CancellationToken ct = default);
 }
