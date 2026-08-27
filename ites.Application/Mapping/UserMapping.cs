74d87234d967c5365feb670680df6642fac5fb00
gurori <nazarovslava124@gmail.com>
2026-08-27 14:06:45 +0300

    refactor: add user mapping

---
+++ ites.Application/Mapping/UserMapping.cs
@@ -0,0 +1,117 @@
+using System.Linq.Expressions;
+using ites.Application.Contracts.Competitions;
+using ites.Application.Contracts.Orders;
+using ites.Application.Contracts.Teams;
+using ites.Application.Contracts.Users;
+using ites.Core.Entities;
+
+namespace ites.Application.Mapping;
+
+public static class UserMapping
+{
+    public static Expression<Func<User, MemberResponse>> ToMemberResponse = u => new MemberResponse(
+        u.Id,
+        u.LastName,
+        u.FirstName,
+        u.MiddleName,
+        u.Email,
+        u.Role,
+        u.Description,
+        u.JobTitle,
+        u.TeamId,
+        u.Competitions.Select(c => new CompetitionSummaryResponse(c.Id, c.ContentInHtml)).ToArray(),
+        u.CompetitionEntries.Select(e => new CompetitionSummaryResponse(
+                e.CompetitionId,
+                e.Competition.ContentInHtml
+            ))
+            .ToArray(),
+        u.Orders.Select(o => new OrderSummaryResponse(
+                o.Id,
+                o.Title,
+                o.Description,
+                o.Price,
+                o.DeadLine
+            ))
+            .ToArray(),
+        u.OrderBids.Select(b => new OrderSummaryResponse(
+                b.OrderId,
+                b.Order.Title,
+                b.Order.Description,
+                b.Order.Price,
+                b.Order.DeadLine
+            ))
+            .ToArray(),
+        u.TeamJoinRequests.Select(j => new TeamSummaryResponse(
+                j.TeamId,
+                j.Team.Name,
+                j.Team.Description,
+                j.Team.Members.Count
+            ))
+            .ToArray()
+    );
+
+    public static Expression<Func<User, ClientResponse>> ToClientResponse = u => new ClientResponse(
+        u.Id,
+        u.LastName,
+        u.FirstName,
+        u.MiddleName,
+        u.Email,
+        u.Role,
+        u.Description,
+        u.JobTitle,
+        u.Orders.Select(o => new OrderSummaryResponse(
+                o.Id,
+                o.Title,
+                o.Description,
+                o.Price,
+                o.DeadLine
+            ))
+            .ToArray(),
+        u.OrderBids.Select(b => new OrderBidResponse(
+                b.Id,
+                new MemberSummaryResponse(
+                    b.UserId,
+                    b.User.LastName,
+                    b.User.FirstName,
+                    b.User.MiddleName,
+                    b.User.Description,
+                    b.User.JobTitle
+                ),
+                new OrderSummaryResponse(
+                    b.OrderId,
+                    b.Order.Title,
+                    b.Order.Description,
+                    b.Order.Price,
+                    b.Order.DeadLine
+                )
+            ))
+            .ToArray()
+    );
+
+    public static Expression<Func<User, OrganizerResponse>> ToOrganizerResponse =
+        u => new OrganizerResponse(
+            u.Id,
+            u.LastName,
+            u.FirstName,
+            u.MiddleName,
+            u.Email,
+            u.Role,
+            u.Description,
+            u.JobTitle,
+            u.Competitions.Select(c => new CompetitionSummaryResponse(c.Id, c.ContentInHtml))
+                .ToArray(),
+            u.CompetitionEntries.Select(e => new CompetitionEntryResponse(
+                    e.Id,
+                    new MemberSummaryResponse(
+                        e.UserId,
+                        e.User.LastName,
+                        e.User.FirstName,
+                        e.User.MiddleName,
+                        e.User.Description,
+                        e.User.JobTitle
+                    ),
+                    new CompetitionSummaryResponse(e.CompetitionId, e.Competition.ContentInHtml)
+                ))
+                .ToArray()
+        );
+}
