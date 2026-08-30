c44b5789a04954c77ccd90e98ad3e75f92c5d904
gurori <nazarovslava124@gmail.com>
2026-08-30 13:29:09 +0300

    fix: update user avatar default path

--- ites.Application/Constants/FileConstants.cs
+++ ites.Application/Constants/FileConstants.cs
@@ -2,5 +2,5 @@ namespace ites.Application.Constants;
 
 public static class FileConstants
 {
-    public const string DefaultAvatarStoragePath = "users/default/avatar.png";
+    public const string DefaultAvatarStoragePath = "users/default/avatar.jpg";
 }
